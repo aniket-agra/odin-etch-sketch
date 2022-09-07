a69e6f1d6eb4dc9a74e96433f22dd7d44783a607
Aniket Agrawal <aniket.mpa@gmail.com>
2022-09-07 19:46:30 +0200

    Fix grid dimensions to produce square divs

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 const grid = document.querySelector('.grid');
-grid.style.cssText = "display: flex; flex-direction: column; width: 960px; height: 600px;";
+grid.style.cssText = "display: flex; flex-direction: column; width: 600px; height: 600px;";
 
 console.log(grid.style.width);
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 10; i++) {
     let gridDivRow = document.createElement('div');
     gridDivRow.style.cssText = "flex: 1 1 auto; display: flex;";
     grid.appendChild(gridDivRow);
-    for (let j = 0; j < 5; j++) {
+    for (let j = 0; j < 10; j++) {
         let gridDivCol = document.createElement('div');
         gridDivCol.style.cssText = "flex: 1 1 auto;";
         gridDivRow.append(gridDivCol);
