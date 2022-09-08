aed0b676ee6dd55dce8ef589d415a55516974519
Aniket Agrawal <aniket.mpa@gmail.com>
2022-09-08 18:32:18 +0200

    Rename variables to improve comprehension, re-order declarations according to logic

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 let numSquares = 64;
+
+// canvas properties must stay fixed regardless of button actions
+const canvas = document.querySelector('.grid');
+// grid must be a square canvas to add div "squares"
+canvas.style.cssText = "display: flex; flex-direction: column; width: 600px; height: 600px;";
+
 const resButton = document.querySelector("button");
 resButton.addEventListener('click', function (e) {
     let userInput = parseInt(prompt("Enter number of squares on one side of grid:", 64));
@@ -7,19 +13,14 @@ resButton.addEventListener('click', function (e) {
     console.log(numSquares);
 });
 
-
-const grid = document.querySelector('.grid');
-// grid must be a square canvas to add div "squares"
-grid.style.cssText = "display: flex; flex-direction: column; width: 600px; height: 600px;";
-
 for (let i = 0; i < numSquares; i++) {
-    let gridDivRow = document.createElement('div');
-    gridDivRow.style.cssText = "flex: 1 1 auto; display: flex;";
-    grid.appendChild(gridDivRow);
+    let canvasRow = document.createElement('div');
+    canvasRow.style.cssText = "flex: 1 1 auto; display: flex;";
+    canvas.appendChild(canvasRow);
     for (let j = 0; j < numSquares; j++) {
-        let gridDivCol = document.createElement('div');
-        gridDivCol.style.cssText = "flex: 1 1 auto;";
-        gridDivRow.append(gridDivCol);
+        let canvasCol = document.createElement('div');
+        canvasCol.style.cssText = "flex: 1 1 auto;";
+        canvasRow.append(canvasCol);
         // gridDivCol.addEventListener('mouseover', function (e) {
         //     gridDivCol.style.backgroundColor = "black";
         // });
