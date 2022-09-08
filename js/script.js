25ec4b80a271899d16407cd2662e79fc7505fefc
Aniket Agrawal <aniket.mpa@gmail.com>
2022-09-08 21:22:44 +0200

    Remove cells and redraw grid when new value is entered

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ canvas.style.cssText = "display: flex; flex-direction: column; width: 600px; hei
 for (let i = 0; i < numSquares; i++) {
     let canvasRow = document.createElement('div');
     canvasRow.style.cssText = "flex: 1 1 auto; display: flex;";
+    canvasRow.classList.add("row");
     canvas.appendChild(canvasRow);
     for (let j = 0; j < numSquares; j++) {
         let canvasCol = document.createElement('div');
@@ -25,6 +26,22 @@ resButton.addEventListener('click', function (e) {
     let userInput = parseInt(prompt("Enter number of squares on one side of grid:", 64));
     // console.log(userInput !== null);
     if (!isNaN(userInput))    numSquares = parseInt(userInput);
+    let rows = document.querySelectorAll(".row");
+    rows.forEach(row => canvas.removeChild(row));
     console.log(numSquares);
+    for (let i = 0; i < numSquares; i++) {
+        let canvasRow = document.createElement('div');
+        canvasRow.style.cssText = "flex: 1 1 auto; display: flex;";
+        canvasRow.classList.add("row");
+        canvas.appendChild(canvasRow);
+        for (let j = 0; j < numSquares; j++) {
+            let canvasCol = document.createElement('div');
+            canvasCol.style.cssText = "flex: 1 1 auto;";
+            canvasRow.append(canvasCol);
+            // gridDivCol.addEventListener('mouseover', function (e) {
+            //     gridDivCol.style.backgroundColor = "black";
+            // });
+        }
+    }
 });
 
